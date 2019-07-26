11f84c5e89f83838ca29eb4d362b385d319b9de9
alechp <me@alechp.com>
2019-07-25 17:31:02 -0700

    initialized repogen config

---
+++ .repogen.js
@@ -0,0 +1,11 @@
+const config = {
+  provider: 'alechp',
+  repospacePath: __dirname,
+  repositories: [
+    {
+      servexyz: 'nommer-lib',
+      servexyz: 'nommer-cli',
+    },
+  ],
+};
+module.exports = config;
